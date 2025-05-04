db2bf305894f3f73f117efe1a1fbcd1ad129c6ff
grantlerduck <daniel.alker@outlook.de>
2025-05-04 16:38:52 +0200

    chore: basic setup

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/grntlrduck-cloud/dynageo
+
+go 1.24.2
+
+require github.com/golang/geo v0.0.0-20250504065118-0e612851eaff
---
+++ internal/geo/consts.go
@@ -0,0 +1,9 @@
+package geo
+
+const (
+	earthRadiusMeter = 6371000.0
+	maxLatitude      = 90.0
+	minLatitude      = -90.0
+	maxLongitude     = 180.0
+	minLongitude     = -180.0
+)
